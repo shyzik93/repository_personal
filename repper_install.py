35a1e2fae24bf3dee02b8b240445413cee264de9
Konstantin Polyakov <shyzik93@mail.ru>
2015-12-05 19:43:05 +0300

    now use repper_install for all python (only repper_install!)

--- repper_install.py
+++ repper_install.py
@@ -10,7 +10,7 @@ if os.sep == '\\': path = path.replace('\\', '\\\\')
 text = text.replace('{DIR_REPOSITORY_PERSONAL}', path)
 
 for path in sys.path:
-  if len(re.findall(r'.+\\Python27\\lib$', path)) != 1: continue
+  if len(re.findall(r'.+\\Python[0-9]{2}\\lib$', path)) != 1: continue
   f = open(os.path.join(path, 'repper.py'), 'w')
   f.write(text)
   f.close()
